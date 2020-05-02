db4dd5a169814877c846268244f9c2d004cea1ec
Shawon Chowdhury <shawon.datascience@gmail.com>
2020-05-02 14:24:33 +0600

    style fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,6 @@
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-        ignore: [`**/\.*`],
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Leukemiabd.org`,
@@ -24,23 +9,5 @@ module.exports = {
         icon: `src/images/LOGO.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    'gatsby-plugin-top-layout',
-    {
-      resolve: 'gatsby-plugin-material-ui',
-      // If you want to use styled components you should change the injection order.
-      options: {
-        // stylesProvider: {
-        //   injectFirst: true,
-        // },
-      },
-    },
-    // If you want to use styled components you should add the plugin here.
-    // 'gatsby-plugin-styled-components',
-    'gatsby-plugin-react-helmet',
   ],
-  siteMetadata: {
-    title: 'আসুন ক্যান্সারকে পরাজিত করি'
-  },
 };
